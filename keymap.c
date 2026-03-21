57f8e063abba444def01184634402c454d183610
d <domenic.mancuso@maine.edu>
2026-03-20 20:24:51 -0400

    added quotation functionality
    Signed-off-by: d <domenic.mancuso@maine.edu>

--- keymap.c
+++ keymap.c
@@ -29,14 +29,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_planck_mit(
         KC_TRNS, KC_F1,     KC_F2,    KC_F3,      KC_F4,     KC_F5,     KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  RM_NEXT,
         KC_TRNS, KC_1,      KC_2,     KC_3,       KC_4,      KC_5,      KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-        KC_TRNS, KC_MINS,   KC_EQL,   KC_LBRC,    KC_RBRC,   KC_BSLS,   KC_GRV,   KC_MPRV, KC_MPLY, KC_MNXT, RM_VALU, KC_TRNS,
+        KC_TRNS, KC_MINS,   KC_EQL,   KC_LBRC,    KC_RBRC,   KC_BSLS,   KC_GRV,   KC_QUOT, KC_MPLY, KC_MNXT, RM_VALU, KC_TRNS,
         KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, RM_VALD, KC_TRNS
     ),
 
     [2] = LAYOUT_planck_mit(
         KC_ESC,  KC_Q,      KC_W,     KC_E,       KC_R,      KC_T,      KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_CAPS, KC_EXLM,   KC_AT,    KC_HASH,    KC_DLR,    KC_PERC,   KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
-        KC_LSFT, S(KC_MINS),S(KC_EQL),S(KC_LBRC), S(KC_RBRC),S(KC_BSLS),S(KC_GRV),KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
+        KC_LSFT, S(KC_MINS),S(KC_EQL),S(KC_LBRC), S(KC_RBRC),S(KC_BSLS),S(KC_GRV),S(KC_QUOT),    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
         KC_LCTL, KC_NO,     KC_LALT,  KC_DEL,     KC_PGDN,   KC_SPC,    KC_PGUP,  KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT
     )
 
